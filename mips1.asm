a0602d6c1d93a0f83ceef45671c92090e46d6669
jasmoncooley <jasmoncooley89@gmail.com>
2018-10-17 22:24:02 -0400

    Fixes the integer printing
    Now prints each integer and adds to the previous registry

--- mips1.asm
+++ mips1.asm
@@ -1,10 +1,22 @@
-.data
-	id: .word 02805055
 .text
-	li $a0, '@'
-	li $v0, 11
-	syscall
-	lw $a0, id
-	li $v0, 1
-	syscall
-
+  main: 
+    li $v0, 11        # Print chars
+    la $a0, 64        # @ (64)
+    syscall
+    sub $v0, $v0,10       # Print integers
+   li $a0, 0       #0 Load integers
+    syscall
+    addi $a0, $a0, 2       # 2
+    syscall
+    addi $a0,$a0, 6       # 8
+    syscall
+    sub $a0,$a0, 8        # 0
+    syscall
+    add $a0,$a0, 5        # 5
+    syscall
+    sub $a0,$a0, 5       # 0
+    syscall
+    add $a0,$a0, 5        # 5
+    syscall
+    addi $a0,$a0, 0       # 5
+    syscall
